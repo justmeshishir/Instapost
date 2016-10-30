56007c9e1d1de482a0588555f787829df028ebea
Shishir Thapa <shishirthapa1@gmail.com>
2016-10-30 05:15:52 +0000

    view post created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,10 @@ class PostsController < ApplicationController
         @post = Post.new
     end
     
+    def show
+      @post = Post.find(params[:id])  
+    end
+    
     def index
       @posts = Post.all.order("created_at DESC")
     end
