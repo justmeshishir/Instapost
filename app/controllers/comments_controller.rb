8c383c0f10818fa0f2729766c7b67df7fc36e7d3
Shishir Thapa <shishirthapa1@gmail.com>
2016-10-31 02:11:37 +0000

    Delete comment added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
     before_action :authenticate_user!, only: :create
+    before_action :is_owner?, only: :destroy
     def create
         @post = Post.find(params[:post_id])
-        @comment = @post.comment.create(comment_params.merge(user_id: current_user.id))
+        @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
         if @comment.valid?
             redirect_to root_path
         else
@@ -11,8 +12,21 @@ class CommentsController < ApplicationController
         end
     end
     
+    def destroy
+      @comment = Comment.find(params[:id])
+      @comment.destroy
+      redirect_to root_path
+    end
+    
     private
     def comment_params
         params.require(:comment).permit(:text, :post_id)
+    end
+    
+    def is_owner?
+      @comment = Comment.find(params[:id])
+      if @comment.user != current_user
+        redirect_to root_path
+      end
     end
 end
