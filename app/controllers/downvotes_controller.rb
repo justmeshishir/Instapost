252fbfba6dbc03e7fcc4a34436ce11fda18acb4c
Shishir Thapa <shishirthapa1@gmail.com>
2016-11-01 03:48:33 +0000

    Ajax added

--- app/controllers/downvotes_controller.rb
+++ app/controllers/downvotes_controller.rb
@@ -5,5 +5,9 @@ class DownvotesController < ApplicationController
     @post = Post.find(params[:post_id])
     @post.downvote_from current_user
     redirect_to posts_path
+    respond_to do |format|
+           format.html { redirect_to posts_path}
+           format.js {}
+        end
   end
 end
